73769a939cadeb82c83533639b8d5620dc6a52dc
김동철 <107951175+csct3434@users.noreply.github.com>
2023-12-12 22:50:14 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,16 +1,10 @@
-import java.util.Arrays;
-
 class Solution {
 
 	public int solution(int[] arr) {
-		int answer = 0;
-
-		Arrays.sort(arr);
 		int LCM = lcm(arr[0], arr[1]);
 		for (int i = 2; i < arr.length; i++) {
 			LCM = lcm(LCM, arr[i]);
 		}
-
 		return LCM;
 	}
 
@@ -19,6 +13,12 @@ class Solution {
 	}
 
 	private int gcm(int a, int b) {
+		if (a > b) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+
 		if (b % a == 0) {
 			return a;
 		}
