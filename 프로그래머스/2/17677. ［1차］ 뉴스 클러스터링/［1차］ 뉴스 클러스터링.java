4799d6ec953e1f4444418bb1665ff550b30ceef3
김동철 <107951175+csct3434@users.noreply.github.com>
2023-12-14 21:08:34 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.49 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,15 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Solution {
 
 	public int solution(String str1, String str2) {
-		Map<String, Long> tokenMap1 = extractTokenMap(str1.toLowerCase());
-		Map<String, Long> tokenMap2 = extractTokenMap(str2.toLowerCase());
+		HashMap<String, Integer> tokenMap1 = new HashMap<>();
+		HashMap<String, Integer> tokenMap2 = new HashMap<>();
 
-		if (tokenMap1.equals(tokenMap2)) {
+		extractTokens(str1.toLowerCase(), tokenMap1);
+		extractTokens(str2.toLowerCase(), tokenMap2);
+
+		if (tokenMap1.size() + tokenMap2.size() == 0) {
 			return 65536;
 		}
 
@@ -18,23 +20,39 @@ class Solution {
 		return 65536 * intersectionSize / unionSize;
 	}
 
-	public Map<String, Long> extractTokenMap(String str) {
-		return IntStream.range(0, str.length() - 1)
-			.mapToObj(i -> str.substring(i, i + 2))
-			.filter(token -> token.chars().allMatch(Character::isAlphabetic))
-			.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	public void extractTokens(String str, Map<String, Integer> tokenMap) {
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (Character.isAlphabetic(str.charAt(i)) && Character.isAlphabetic(str.charAt(i + 1))) {
+				String token = str.substring(i, i + 2);
+				Integer count = tokenMap.getOrDefault(token, 0);
+				tokenMap.put(token, count + 1);
+			}
+		}
 	}
 
-	public int calcIntersectionSize(Map<String, Long> tokenMap1, Map<String, Long> tokenMap2) {
-		return tokenMap1.keySet().stream()
-			.filter(tokenMap2::containsKey)
-			.map(key -> Math.min(tokenMap1.getOrDefault(key, 0L), tokenMap2.getOrDefault(key, 0L)))
-			.mapToInt(Long::intValue)
-			.sum();
+	public int calcIntersectionSize(Map<String, Integer> tokenMap1, Map<String, Integer> tokenMap2) {
+		int size = 0;
+
+		for (String key : tokenMap1.keySet()) {
+			if (tokenMap2.containsKey(key)) {
+				size += Math.min(tokenMap1.get(key), tokenMap2.get(key));
+			}
+		}
+
+		return size;
 	}
 
-	public int calcUnionSize(Map<String, Long> map1, Map<String, Long> map2) {
-		map1.forEach((key, value) -> map2.put(key, Math.max(value, map2.getOrDefault(key, 0L))));
-		return map2.values().stream().mapToInt(Long::intValue).sum();
+	public int calcUnionSize(Map<String, Integer> map1, Map<String, Integer> map2) {
+		int size = 0;
+		
+		HashSet<String> allKeys = new HashSet<>();
+		allKeys.addAll(map1.keySet());
+		allKeys.addAll(map2.keySet());
+
+		for (String key : allKeys) {
+			size += Math.max(map1.getOrDefault(key, 0), map2.getOrDefault(key, 0));
+		}
+		
+		return size;
 	}
 }
