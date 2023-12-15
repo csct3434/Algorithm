d34619f602dadb27acdff769f4ed7065d269878f
김동철 <107951175+csct3434@users.noreply.github.com>
2023-12-16 03:35:40 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 13.13 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,28 +1,19 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 class Solution {
 
 	public String[] solution(String[] files) {
-		HashMap<String, Integer> initialOrder = new HashMap<>();
-		for (int i = 0; i < files.length; i++) {
-			initialOrder.put(files[i], i);
-		}
-
-		return Arrays.stream(files)
-			.sorted((str1, str2) -> {
-				String head1 = parseHead(str1).toLowerCase();
-				String head2 = parseHead(str2).toLowerCase();
+		Arrays.sort(files, (str1, str2) -> {
+				String head1 = parseHead(str1.toLowerCase());
+				String head2 = parseHead(str2.toLowerCase());
 				if (head1.compareTo(head2) == 0) {
 					Integer number1 = parseNumber(str1.substring(head1.length()));
 					Integer number2 = parseNumber(str2.substring(head2.length()));
-					if (number1.compareTo(number2) == 0) {
-						return initialOrder.get(str1) - initialOrder.get(str2);
-					}
 					return number1.compareTo(number2);
 				}
 				return head1.compareTo(head2);
-			}).toArray(String[]::new);
+			});
+		return files;
 	}
 
 	private String parseHead(String str) {
