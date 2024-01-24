165141ff6a33350fb53a6c358ba48ece01f7163d
김동철 <107951175+csct3434@users.noreply.github.com>
2024-01-24 14:37:07 +0900

    [level 2] Title: 튜플, Time: 4.97 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -5,14 +5,18 @@ import java.util.HashSet;
 class Solution {
 
     public int[] solution(String s) {
+        int[] answer;
+        int count = 0;
+
         String[] subsets = s.replaceAll("},", "} ").substring(1, s.length() - 1).split(" ");
         Arrays.sort(subsets, Comparator.comparingInt(String::length));
 
-        int[] answer = new int[subsets.length];
-        int count = 0;
+        answer = new int[subsets.length];
         HashSet<String> used = new HashSet<>();
+
         for (String subset : subsets) {
             String nextNumber = Arrays.stream(subset.substring(1, subset.length() - 1).split(","))
+                .parallel()
                 .filter(number -> !used.contains(number))
                 .findFirst()
                 .orElse("0");
