48731a5498031ffb10597b9f6bc8925a0d9d3af6
김동철 <107951175+csct3434@users.noreply.github.com>
2024-03-05 22:40:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 184.99 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -6,30 +6,14 @@ class Solution {
         int total = elements.length;
         HashSet<Integer> sequenceSum = new HashSet<>();
 
-        int[] sum = new int[total];
-        sum[0] = elements[0];
-        for (int i = 1; i < total; i++) {
-            sum[i] = sum[i - 1] + elements[i];
-        }
-
-        for (int length = 1; length <= total; length++) {
-            for (int start = 0; start < total; start++) {
-                sequenceSum.add(calcSequenceSum(elements, total, sum, start, length));
+        int[] dp = new int[total + 1];
+        for (int idx = 0; idx < total; idx++) {
+            for (int length = 1; length <= total; length++) {
+                dp[length] = dp[length - 1] + elements[(idx + length - 1) % total];
+                sequenceSum.add(dp[length]);
             }
         }
 
         return sequenceSum.size();
-    }
-
-    private int calcSequenceSum(int[] elements, int total, int[] sum, int start, int length) {
-        int end = (start + length - 1) % total;
-
-        if (start == end) {
-            return elements[start];
-        } else if (start < end) {
-            return sum[end] - sum[start] + elements[start];
-        } else {
-            return sum[end] + (sum[total - 1] - sum[start - 1]);
-        }
     }
 }
