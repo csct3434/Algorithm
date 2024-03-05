f515e9d2a092c91f1ded80121b32055309c69b8c
김동철 <107951175+csct3434@users.noreply.github.com>
2024-03-05 23:45:49 +0900

    [level 2] Title: 점 찍기, Time: 0.18 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,9 +2,10 @@ class Solution {
 
     public long solution(int k, int d) {
         long answer = 0;
-
-        for (int x = 0; x <= d; x += k) {
-            answer += 1 + ((int) Math.sqrt(1D * d * d - 1D * x * x) / k);
+        long dSquare = (long) d * d;
+        
+        for (long x = 0; x <= d; x += k) {
+            answer += 1 + ((int) Math.sqrt(dSquare - x * x) / k);
         }
 
         return answer;
