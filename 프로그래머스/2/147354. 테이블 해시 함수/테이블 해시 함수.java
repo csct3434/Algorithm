7095307c783f88dccec525bd8e1f1e4ed34e8ee0
김동철 <107951175+csct3434@users.noreply.github.com>
2024-03-07 08:13:48 +0900

    [level 2] Title: 테이블 해시 함수, Time: 3.14 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -3,21 +3,11 @@ import java.util.stream.IntStream;
 
 class Solution {
 
-    public int solution(int[][] data, int _col, int row_begin, int row_end) {
-        final int col = _col - 1;
-
-        Arrays.sort(data, (a, b) -> {
-            if (a[col] == b[col]) {
-                return b[0] - a[0];
-            }
-            return a[col] - b[col];
-        });
+    public int solution(int[][] data, int col, int row_begin, int row_end) {
+        Arrays.sort(data, (a, b) -> a[col - 1] == b[col - 1] ? b[0] - a[0] : a[col - 1] - b[col - 1]);
 
         return IntStream.rangeClosed(row_begin, row_end)
-            .map(i -> {
-                final int fi = i;
-                return Arrays.stream(data[i - 1]).map(x -> x % fi).sum();
-            })
+            .map(i -> Arrays.stream(data[i - 1]).map(x -> x % i).sum())
             .reduce(0, (a, b) -> a ^ b);
     }
 }
