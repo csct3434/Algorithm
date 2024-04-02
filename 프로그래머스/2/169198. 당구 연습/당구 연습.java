ae2fc7425f73846d614a0623fb34340a497fe3a6
김동철 <107951175+csct3434@users.noreply.github.com>
2024-04-03 04:18:32 +0900

    [level 2] Title: 당구 연습, Time: 0.64 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -10,28 +10,36 @@ class Solution {
     private int solve(int m, int n, int startX, int startY, int targetX, int targetY) {
         double dist = Integer.MAX_VALUE;
         
+        // 위쪽
         if((startX != targetX) || (startY > targetY)) {
             dist = Math.min(dist, Math.pow(startX - targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
         }
+        // 왼쪽
         if((startY != targetY) || (startX < targetX)) {
             dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(startY - targetY, 2));
         }
+        // 아래쪽
         if((startX != targetX) || (startY < targetY)) {
             dist = Math.min(dist, Math.pow(startX - targetX, 2) + Math.pow(startY + targetY, 2));
         }
+        // 오른쪽
         if((startY != targetY) || (startX > targetX)) {
             dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(startY - targetY, 2));
         }
         
+        // 왼쪽 위 대각선
         if(tan(startX, n - startY) == tan(targetX, n - targetY) && startX < targetX) {
             dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
         }
+        // 오른쪽 위 대각선
         if(tan(m - startX, n - startY) == tan(m - targetX, n - targetY) && startX < targetX) {
             dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
         }
+        // 왼쪽 아래 대각선
         if(tan(startX, startY) == tan(targetX, targetY) && startX < targetX) {
             dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(startY + targetY, 2));
         }
+        // 오른쪽 아래 대각선
         if(tan(m - startX, startY) == tan(m - targetX, targetY) && startX > targetX) {
             dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(startY + targetY, 2));
         }
