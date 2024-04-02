2c2c3002fdb711695e00ad30ef159770c1b21374
김동철 <107951175+csct3434@users.noreply.github.com>
2024-04-03 04:15:40 +0900

    [level 2] Title: 당구 연습, Time: 0.81 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+        for(int i=0; i<balls.length; i++) {
+            answer[i] = solve(m, n, startX, startY, balls[i][0], balls[i][1]);
+        }
+        return answer;
+    }
+    
+    private int solve(int m, int n, int startX, int startY, int targetX, int targetY) {
+        double dist = Integer.MAX_VALUE;
+        
+        if((startX != targetX) || (startY > targetY)) {
+            dist = Math.min(dist, Math.pow(startX - targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
+        }
+        if((startY != targetY) || (startX < targetX)) {
+            dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(startY - targetY, 2));
+        }
+        if((startX != targetX) || (startY < targetY)) {
+            dist = Math.min(dist, Math.pow(startX - targetX, 2) + Math.pow(startY + targetY, 2));
+        }
+        if((startY != targetY) || (startX > targetX)) {
+            dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(startY - targetY, 2));
+        }
+        
+        if(tan(startX, n - startY) == tan(targetX, n - targetY) && startX < targetX) {
+            dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
+        }
+        if(tan(m - startX, n - startY) == tan(m - targetX, n - targetY) && startX < targetX) {
+            dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(2 * n - startY - targetY, 2));
+        }
+        if(tan(startX, startY) == tan(targetX, targetY) && startX < targetX) {
+            dist = Math.min(dist, Math.pow(startX + targetX, 2) + Math.pow(startY + targetY, 2));
+        }
+        if(tan(m - startX, startY) == tan(m - targetX, targetY) && startX > targetX) {
+            dist = Math.min(dist, Math.pow(2 * m - startX - targetX, 2) + Math.pow(startY + targetY, 2));
+        }
+        
+        return (int) dist;
+    }
+    
+    private double tan(double x, double y) {
+        return y / x;
+    }
+}
