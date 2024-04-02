4e85445c7bd674ba975ccc6cb44d7fae270903a7
김동철 <107951175+csct3434@users.noreply.github.com>
2024-04-03 03:23:43 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
@@ -1,10 +1,10 @@
 # [level 2] 유사 칸토어 비트열 - 148652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652#) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.04 ms
+메모리: 75.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 02:59:14
+2024년 04월 03일 03:23:42
 
 ### 문제 설명
 
--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,28 +1,18 @@
-import java.util.stream.*;
-
 class Solution {
     public int solution(int n, long l, long r) {
-        long total = (long) Math.pow(4, n);
-        if(l > 1)   total -= solve(n, l - 2);
-        if(r < Math.pow(5, n))  total -= solve(n, (long) Math.pow(5, n) - r - 1);
-        return (int) total;
+        if(n == 0)  return 1;
+        return (int) (solve(n, r-1) - solve(n, l-2));
     }
     
-    private long solve(int n, long r) {
-        if(n == 1) {
-            return r < 2 ? r + 1 : r;
-        }
+    public long solve(int n, long r) {
+        if(n == 1)  return r < 2 ? r + 1 : r;
         
-        long count = 0;
-        long subLen = (long) Math.pow(5, n-1);
+        long group = r / (long) Math.pow(5, n-1);
+        long pos = r % (long) Math.pow(5, n-1);
+        long subCount = (long) Math.pow(4, n-1);
         
-        long subGroup = r / subLen;
-        count += Math.pow(4, n-1) * (subGroup <= 2 ? subGroup : subGroup - 1);
-        
-        if(r < subLen * 2 || r >= subLen * 3) {
-            count += solve(n-1, r % subLen);
-        }
-        
-        return count;
+        if(group == 2)  return subCount * 2;
+        if(group < 2)   return subCount * group + solve(n-1, pos);
+        return subCount * --group + solve(n-1, pos);
     }
 }
