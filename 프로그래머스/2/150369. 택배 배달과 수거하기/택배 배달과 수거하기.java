a843dea0e82707bb692140e71e9d943e67d3905c
김동철 <107951175+csct3434@users.noreply.github.com>
2024-04-18 11:19:46 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 41.99 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -4,36 +4,35 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
         
-        PriorityQueue<int[]> deliveryPQ = new PriorityQueue<>(Comparator.comparingInt(a -> -a[0]));
-        PriorityQueue<int[]> pickupPQ = new PriorityQueue<>(Comparator.comparingInt(a -> -a[0]));
+        Stack<int[]> dS = new Stack<>();
+        Stack<int[]> pS = new Stack<>();
         
         for(int i=0; i<deliveries.length; i++) {
-            if(deliveries[i] > 0) deliveryPQ.add(new int[]{i+1, deliveries[i]});
-            if(pickups[i] > 0) pickupPQ.add(new int[]{i+1, pickups[i]});
+            if(deliveries[i] > 0) dS.push(new int[]{i+1, deliveries[i]});
+            if(pickups[i] > 0) pS.push(new int[]{i+1, pickups[i]});
         }
         
-        while(!(deliveryPQ.isEmpty() && pickupPQ.isEmpty())) {
+        while(!(dS.isEmpty() && pS.isEmpty())) {
             int capacity = cap;
-            answer += 2 * Math.max(deliveryPQ.isEmpty() ? 0 : deliveryPQ.peek()[0], 
-                               pickupPQ.isEmpty() ? 0 : pickupPQ.peek()[0]);
+            answer += 2 * Math.max(dS.isEmpty() ? 0 : dS.peek()[0], pS.isEmpty() ? 0 : pS.peek()[0]);
             
-            while(!deliveryPQ.isEmpty() && capacity > 0) {
-                if(deliveryPQ.peek()[1] <= capacity) {
-                    capacity -= deliveryPQ.peek()[1];
-                    deliveryPQ.poll();
+            while(!dS.isEmpty() && capacity > 0) {
+                if(dS.peek()[1] <= capacity) {
+                    capacity -= dS.peek()[1];
+                    dS.pop();
                 } else {
-                    deliveryPQ.peek()[1] -= capacity;
+                    dS.peek()[1] -= capacity;
                     capacity = 0;
                 }
             }
             
             capacity = cap;
-            while(!pickupPQ.isEmpty() && capacity > 0) {
-                if(pickupPQ.peek()[1] <= capacity) {
-                    capacity -= pickupPQ.peek()[1];
-                    pickupPQ.poll();
+            while(!pS.isEmpty() && capacity > 0) {
+                if(pS.peek()[1] <= capacity) {
+                    capacity -= pS.peek()[1];
+                    pS.pop();
                 } else {
-                    pickupPQ.peek()[1] -= capacity;
+                    pS.peek()[1] -= capacity;
                     capacity = 0;
                 }
             }
