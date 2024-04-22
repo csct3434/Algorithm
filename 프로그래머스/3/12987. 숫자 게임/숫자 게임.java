f4e324e9197e76799b844f4f09c08018d39960e4
김동철 <107951175+csct3434@users.noreply.github.com>
2024-04-22 23:52:43 +0900

    [level 3] Title: 숫자 게임, Time: 63.03 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -3,17 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
-        int left = 0, right = B.length - 1;
         
         Arrays.sort(A);
         Arrays.sort(B);
         
-        for(int i = A.length - 1; i >= 0 && left <= right; i--) {
-            if(A[i] < B[right]) {
+        for(int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
+            if(A[i] < B[j]) {
                 answer++;
-                right--;
-            } else {
-                left++;
+                j--;
             }
         }
         
