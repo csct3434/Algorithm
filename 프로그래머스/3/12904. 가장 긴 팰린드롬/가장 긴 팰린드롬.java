2f0e02659fa1c42977c7150940969e40a8874fea
김동철 <107951175+csct3434@users.noreply.github.com>
2024-05-14 10:47:42 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 21.09 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int solution(String s) {
+        int answer = -1, len = s.length();
+
+        for (int i=0; i < len; i++) {
+            for (int j=1; i-j >= 0 && i+j < len; j++) {
+                if (s.charAt(i-j) == s.charAt(i+j)) {
+                    answer = Math.max(answer, j * 2 + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0; i<len; i++) {
+            for(int j=1; i-j+1 >=0 && i+j < len; j++) {
+                if(s.charAt(i-j+1) == s.charAt(i+j)) {
+                    answer = Math.max(answer, j * 2);
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        if(answer == -1) {
+            return 1;
+        }
+        return answer;
+    }
+}
