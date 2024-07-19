f2d35e54b8e8dd3e6d769d47069f05ff8db044a2
김동철 <107951175+csct3434@users.noreply.github.com>
2024-07-19 15:48:02 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(long num) {
+        int count = 0;
+        while(num != 1 && ++count <= 500) {
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+        }
+        return count > 500 ? -1 : count;
+    }
+}
