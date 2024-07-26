8ed433f0833664912f9de7a654820f2befc740bb
김동철 <107951175+csct3434@users.noreply.github.com>
2024-07-26 20:38:33 +0900

    [level 1] Title: 예산, Time: 0.59 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -4,8 +4,7 @@ class Solution {
     public int solution(int[] d, int budget) {
         Arrays.sort(d);
         for(int i=0; i<d.length; i++) {
-            budget -= d[i];
-            if(budget < 0) return i;
+            if((budget -= d[i]) < 0) return i;
         }
         return d.length;
     }
