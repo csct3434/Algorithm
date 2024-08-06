4043c57d429c39dba56ece7692bb682f0738ffe5
김동철 <107951175+csct3434@users.noreply.github.com>
2024-08-07 00:00:48 +0900

    [level 1] Title: 과일 장수, Time: 7.59 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,19 +4,10 @@ class Solution {
         for(int s : score) {
             count[s]++;
         }
-        int answer = 0, remainder = 0;
+        int answer = 0;
         for(int s=k; s>=1; s--) {
-            if(remainder > 0) {
-                if(count[s] < m - remainder) {
-                    remainder += count[s];
-                    continue;
-                }
-                count[s] -= m - remainder;
-                answer += s * m;
-                remainder = 0;
-            }
             answer += (count[s] / m) * s * m;
-            remainder = count[s] % m;
+            count[s-1] += count[s] % m;
         }
         return answer;
     }
