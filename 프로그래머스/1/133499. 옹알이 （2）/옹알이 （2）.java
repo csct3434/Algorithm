2e63a08d86a00a2471fc3e0374fd72dd5927e437
김동철 <107951175+csct3434@users.noreply.github.com>
2024-08-19 15:48:47 +0900

    [level 1] Title: 옹알이 (2), Time: 4.61 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,30 +1,12 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] units = new String[]{"aya", "ye", "woo", "ma"};
         for(String word : babbling) {
-            int idx = 0;
-            String lastUnit = "";
-            boolean success = true;
-            while(idx < word.length()) {
-                boolean matched = false;
-                for(String unit : units) {
-                    if(!lastUnit.equals(unit) 
-                       && idx + unit.length() <= word.length() 
-                       && word.substring(idx, idx + unit.length()).equals(unit)
-                      ) {
-                        lastUnit = unit;
-                        idx += unit.length();
-                        matched = true;
-                        break;
-                    }
-                }
-                if(!matched) {
-                    success = false;
-                    break; 
-                }
+            if(word.contains("ayaaya") || word.contains("yeye") 
+               || word.contains("woowoo") || word.contains("mama")) {
+                continue;
             }
-            if(success) answer++;
+            if(word.replaceAll("aya|ma|ye|woo", " ").replace(" ", "").equals("")) answer++;
         }
         return answer;
     }
