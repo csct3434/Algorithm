e6a8a19789e9d239c71a082988884abe21adfdf7
김동철 <107951175+csct3434@users.noreply.github.com>
2024-08-19 15:06:26 +0900

    [level 1] Title: 실패율, Time: 5.67 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] count = new int[N+2];
+        for(int stage : stages) {
+            count[stage]++;
+        }
+        int total = stages.length;
+        List<int[]> failRate = new ArrayList<>();
+        for(int stage = 1; stage <= N; stage++) {
+            failRate.add(new int[]{stage, count[stage], total});
+            total -= count[stage];
+        }
+        failRate.sort((a, b) -> {
+            if(b[2] == 0) {
+                if(a[2] == 0) return a[0] - b[0];
+                return -1;
+            }
+            if((long) a[1] * b[2] == (long) b[1] * a[2]) return a[0] - b[0];
+            return (long) b[1] * a[2] - (long) a[1] * b[2] < 0 ? -1 : 1;
+        });
+        int[] answer = new int[N];
+        for(int i=0; i<N; i++) {
+            answer[i] = failRate.get(i)[0];
+        }
+        return answer;
+    }
+}
