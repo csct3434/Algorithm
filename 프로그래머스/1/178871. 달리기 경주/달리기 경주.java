4ecf876679571a898cee8e9cb618441336be6ab4
김동철 <107951175+csct3434@users.noreply.github.com>
2024-08-27 23:22:23 +0900

    [level 1] Title: 달리기 경주, Time: 307.31 ms, Memory: 361 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        Map<String, Integer> rank = new HashMap<>();
+        for(int i=0; i<players.length; i++) {
+            rank.put(players[i], i);
+            answer[i] = players[i];
+        }
+        for(String calling : callings) {
+            int idx = rank.get(calling);
+            String temp = answer[idx];
+            answer[idx] = answer[idx - 1];
+            answer[idx - 1] = temp;
+            rank.put(answer[idx], idx);
+            rank.put(answer[idx - 1], idx - 1);
+        }
+        return answer;
+    }
+}
