89b84ba6a9625c5dcdf6b88698775695e60a5f72
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-01 22:14:38 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 492 ms, Memory: 48096 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// 크루스칼 알고리즘
+public class Main {
+
+    private static final int[] parent = new int[10001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken());
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            pq.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+        Arrays.fill(parent, -1);
+        int answer = 0, connection = 0, p1, p2;
+        while (!pq.isEmpty()) {
+            int[] info = pq.remove();
+            p1 = find(info[0]);
+            p2 = find(info[1]);
+            if (p1 == p2) continue;
+            answer += info[2];
+            union(p1, p2);
+            if (++connection == V - 1) break;
+        }
+        System.out.print(answer);
+    }
+
+    private static int find(int v) {
+        if (parent[v] < 0) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    private static void union(int v1, int v2) {
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if (p1 == p2) return;
+        if (parent[p1] < parent[p2]) {
+            parent[p1] += parent[p2];
+            parent[p2] = p1;
+        } else {
+            parent[p2] = parent[p1];
+            parent[p1] = p2;
+        }
+    }
+}
