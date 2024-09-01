c282dfe5be0481b9676efb683203e099a2423d7d
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-02 01:06:48 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.24 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,10 +3,8 @@ class Solution {
         StringBuilder answer = new StringBuilder();
         int[] score = new int[26];
         for(int i=0; i<survey.length; i++) {
-            String type = survey[i];
-            int choice = choices[i];
-            if(choice == 4) continue;
-            score[type.charAt(choice / 4) - 'A'] += Math.abs(4 - choice);
+            if(choices[i] == 4) continue;
+            score[survey[i].charAt(choices[i] / 4) - 'A'] += Math.abs(4 - choices[i]);
         }
         for(String type : new String[]{"RT", "CF", "JM", "AN"}) {
             answer.append(score[type.charAt(0) - 'A'] >= score[type.charAt(1) - 'A'] ? type.charAt(0) : type.charAt(1));
