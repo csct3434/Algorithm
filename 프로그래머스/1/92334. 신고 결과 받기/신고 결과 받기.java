9281083e168b26c7a2c4a79b7787c124e2f05116
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-04 09:40:01 +0900

    [level 1] Title: 신고 결과 받기, Time: 252.66 ms, Memory: 225 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -5,15 +5,9 @@ class Solution {
         int[] answer = new int[id_list.length], reportCount = new int[id_list.length];
         Map<String, Integer> indexMap = new HashMap<>();
         for(int i=0; i<id_list.length; i++) indexMap.put(id_list[i], i);
-        Set<String> reportSet = new HashSet<>();
-        for(String r : report) reportSet.add(r);
-        for(String r : reportSet) reportCount[indexMap.get(r.split(" ")[1])]++;
-        for(String r : reportSet) {
-            String[] token = r.split(" ");
-            if(reportCount[indexMap.get(token[1])] >= k) {
-                answer[indexMap.get(token[0])]++;
-            }
-        }
+        Arrays.stream(report).distinct().forEach(s -> reportCount[indexMap.get(s.split(" ")[1])]++);
+        Arrays.stream(report).distinct().filter(s -> reportCount[indexMap.get(s.split(" ")[1])] >= k)
+            .forEach(t -> answer[indexMap.get(t.split(" ")[0])]++);
         return answer;
     }
 }
