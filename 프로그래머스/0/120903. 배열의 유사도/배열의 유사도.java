122b994be66e8a113f0d41c2894b21f4c4c6df86
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-13 23:08:34 +0900

    [level 0] Title: 배열의 유사도, Time: 0.19 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-        Set<String> set = new HashSet<>();
-        for(String s : s1) set.add(s);
-        int answer = 0;
-        for(String s : s2) if(set.contains(s)) answer++;
-        return answer;
+        Set<String> set = new HashSet<>(Arrays.asList(s1));
+        set.retainAll(Arrays.asList(s2));
+        return set.size();
     }
 }
