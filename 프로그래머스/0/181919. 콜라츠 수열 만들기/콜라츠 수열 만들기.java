e99aab9a7eb23727ea1b0a5a723dc41d66d39206
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-13 23:59:25 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 3.06 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        while(n > 1) {
+            list.add(n);
+            n = n % 2 == 0 ? n / 2 : 3 * n + 1;
+        }
+        list.add(1);
+        return list.stream().mapToInt(a -> a).toArray();
+    }
+}
