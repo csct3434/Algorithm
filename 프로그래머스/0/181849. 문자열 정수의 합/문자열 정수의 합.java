1c989d463ab5ae98d59557a7a139c1154d0edd8f
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-13 23:33:09 +0900

    [level 0] Title: 문자열 정수의 합, Time: 3.87 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -1,7 +1,5 @@
-import java.util.*;
-
 class Solution {
     public int solution(String num_str) {
-        return Arrays.stream(num_str.split("")).mapToInt(Integer::parseInt).sum();
+        return num_str.chars().map(n -> n - '0').sum();
     }
 }
