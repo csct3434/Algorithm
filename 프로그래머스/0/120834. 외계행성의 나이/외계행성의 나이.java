e5b724fc70bfaa6bec6faf18996d662d805773ad
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-15 01:33:22 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.15 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int age) {
+        StringBuilder ans = new StringBuilder();
+        while(age > 0) {
+            ans.insert(0, Character.valueOf((char) ('a' + (age % 10))));
+            age /= 10;
+        }
+        return ans.toString();
+    }
+}
