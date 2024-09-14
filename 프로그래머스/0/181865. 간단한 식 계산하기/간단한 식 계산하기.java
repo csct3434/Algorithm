72ba89995e43e1647a8123145a0380f5461488e4
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-15 00:56:13 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,10 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(String binomial) {
-        int[] num = Arrays.stream(binomial.replace(" " , "").split("[+*-]")).mapToInt(Integer::parseInt).toArray();
-        if(binomial.indexOf("+") > 0) return num[0] + num[1];
-        if(binomial.indexOf("-") > 0) return num[0] - num[1];
-        return num[0] * num[1];
+        String[] token = binomial.split(" ");
+        int num1 = Integer.parseInt(token[0]), num2 = Integer.parseInt(token[2]);
+        if(token[1].equals("+")) return num1 + num2;
+        if(token[1].equals("-")) return num1 - num2;
+        return num1 * num2;
     }
 }
