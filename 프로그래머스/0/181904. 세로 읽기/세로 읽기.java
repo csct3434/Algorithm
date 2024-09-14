b7a682aa4b7564e536d72d1a2e1a480da7134125
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-15 01:49:07 +0900

    [level 0] Title: 세로 읽기, Time: 0.16 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder ans = new StringBuilder();
+        for(int i = c-1; i < my_string.length(); i += m) ans.append(my_string.charAt(i));
+        return ans.toString();
+    }
+}
