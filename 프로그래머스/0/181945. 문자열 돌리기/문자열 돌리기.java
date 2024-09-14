307bfb1e28e5c96a45e0b6236c20a75254835bc1
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-15 01:16:49 +0900

    [level 0] Title: 문자열 돌리기, Time: 150.93 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Arrays.stream(new Scanner(System.in).next().split("")).forEach(System.out::println);
+    }
+}
