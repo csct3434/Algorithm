ef56412fce86276548fdf9936534a1ebe4907e07
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-14 23:13:57 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,7 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2 - num1 + 1];
-        System.arraycopy(numbers, num1, answer, 0, num2 - num1 + 1);
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
