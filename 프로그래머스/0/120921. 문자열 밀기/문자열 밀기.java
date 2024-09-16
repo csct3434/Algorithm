e5b2b674ddc28adf72fe51a6e89ec478797293a4
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-17 04:30:38 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        int len = A.length();
-        char[] c = A.toCharArray();
-        for(int i=0; i<len; i++) {
-            char[] temp = new char[len];
-            for(int j=0; j<len; j++) temp[(j + i) % len] = c[j];
-            if (new String(temp).equals(B)) return i;
-        }
-        return -1;
+        return B.repeat(2).indexOf(A);
     }
 }
