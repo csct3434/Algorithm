1a94473f87888eba03eabee3ce1b21d60217dacd
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-19 02:00:30 +0900

    [level 0] Title: 그림 확대, Time: 0.37 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        for(int i=0; i<picture.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for(char c : picture[i].toCharArray()) {
+                for(int j=0; j<k; j++) sb.append(c);
+            }
+            for(int j=0; j<k; j++) answer[i * k + j] = sb.toString();
+        }
+        return answer;
+    }
+}
