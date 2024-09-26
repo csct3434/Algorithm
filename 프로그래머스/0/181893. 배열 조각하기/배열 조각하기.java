1415ea62fd2cebaffb71664da8f052a01afd2382
김동철 <107951175+csct3434@users.noreply.github.com>
2024-09-27 02:01:06 +0900

    [level 0] Title: 배열 조각하기, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int l = 0, r = arr.length - 1;
+        for(int i=0; i<query.length; i++) {
+            if(i % 2 == 0) r = l + query[i];
+            else l += query[i];
+        }
+        return Arrays.copyOfRange(arr, l, r+1);
+    }
+}
