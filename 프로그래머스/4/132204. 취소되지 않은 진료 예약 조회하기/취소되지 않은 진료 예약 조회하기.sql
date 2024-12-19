fbafa8abf05f2b2ae081ecf3b7bdc1de5e10cd79
김동철 <csct3434@gmail.com>
2024-12-19 19:10:32 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,19 @@
+SELECT
+    a.APNT_NO,
+    p.PT_NAME,
+    p.PT_NO,
+    a.MCDP_CD,
+    d.DR_NAME,
+    a.APNT_YMD
+FROM
+    APPOINTMENT a
+JOIN
+    PATIENT p USING (PT_NO)
+JOIN
+    DOCTOR d ON a.MDDR_ID = d.DR_ID
+WHERE
+    a.MCDP_CD = 'CS'
+    AND a.APNT_YMD LIKE '2022-04-13%'
+    AND a.APNT_CNCL_YN = 'N'
+ORDER BY
+    a.APNT_YMD ASC;
