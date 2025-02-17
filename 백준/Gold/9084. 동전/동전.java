428b9779b1d1eae5f67a10be1280d24421faafd8
김동철 <csct3434@gmail.com>
2025-02-17 22:40:06 +0900

    [Gold V] Title: 동전, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder answer = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+
+        while(T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[] coins = new int[N+1];
+            st = new StringTokenizer(br.readLine());
+            for(int i=1; i<=N; i++) {
+                coins[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int M = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[M+1];
+            dp[0] = 1;
+
+            for(int i=1; i<=N; i++) {
+                for(int j=coins[i]; j<=M; j++) {
+                    dp[j] += dp[j-coins[i]];
+                }
+            }
+
+            answer.append(dp[M]).append('\n');
+        }
+        System.out.print(answer);
+    }
+}
