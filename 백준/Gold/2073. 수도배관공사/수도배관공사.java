a2aff853f6b62b977d8a59abcc1dd8a950b49e02
김동철 <csct3434@gmail.com>
2025-02-23 18:07:57 +0900

    [Gold IV] Title: 수도배관공사, Time: 188 ms, Memory: 17372 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.java
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     private static final int UNREACHABLE = -1;
 
     public static void main(String[] args) throws IOException {
@@ -11,11 +10,11 @@ public class Main {
 
         int D = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
-        
-        int[] L = new int[P + 1];
-        int[] C = new int[P + 1];
-        
-        for (int i = 1; i <= P; i++) {
+
+        int[] L = new int[P];
+        int[] C = new int[P];
+
+        for (int i = 0; i < P; i++) {
             st = new StringTokenizer(br.readLine());
             L[i] = Integer.parseInt(st.nextToken());
             C[i] = Integer.parseInt(st.nextToken());
@@ -23,19 +22,17 @@ public class Main {
 
         int[] dp = new int[D + 1];
         Arrays.fill(dp, UNREACHABLE);
-        dp[0] = 0;
+        dp[0] = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= P; i++) {
-            int length = L[i];
-            int capacity = C[i];
-            for (int j = D; j > length; j--) {
+        for (int i = 0; i < P; i++) {
+            int length = L[i], capacity = C[i];
+            for (int j = D; j >= length; j--) {
                 if (dp[j - length] != UNREACHABLE) {
                     dp[j] = Math.max(dp[j], Math.min(dp[j - length], capacity));
                 }
             }
-            dp[length] = Math.max(dp[length], capacity);
         }
 
-        System.out.print(dp[D]);
+        System.out.println(dp[D]);
     }
 }
