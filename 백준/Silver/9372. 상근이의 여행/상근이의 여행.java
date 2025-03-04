05af8ca65147b11ef36c1a0b1ccdb435acaf069e
김동철 <csct3434@gmail.com>
2025-03-04 12:17:19 +0900

    [Silver IV] Title: 상근이의 여행, Time: 212 ms, Memory: 24036 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            while (M-- > 0) {
+                br.readLine();
+            }
+            System.out.println(N - 1);
+        }
+    }
+}
